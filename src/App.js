790f2ac7246b15f76ecbe21afc994196e5da33b4
sujitashokdhumale <sujitashokdhumale@gmail.com>
2025-05-14 23:48:30 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Heatmax CRM MVP</h1>
+      <p>Quote builder interface placeholder.</p>
+    </div>
+  );
+}
+
+export default App;
